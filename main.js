1b366134d23ccc2ce10432042a322b083fa53baa
Bob <bob.e.baxter@gmal.com>
2019-03-02 13:46:40 -0600

    wrote printToDom function

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-const firstName = 'Bob';
-const lastName = 'Baxter';
-// console.log(firstName + ' ' + lastName);
-// console.log(`${firstName} ${lastName}`);
+// const firstName = 'Bob';
+// const lastName = 'Baxter';
+// // console.log(firstName + ' ' + lastName);
+// // console.log(`${firstName} ${lastName}`);
 
-const namePrinter = (firstName, lastName) => {
-    console.log(`${firstName} ${lastName}`)
-}
+// const namePrinter = (firstName, lastName) => {
+//     console.log(`${firstName} ${lastName}`)
+// }
 
-namePrinter('Bob', 'Baxter');
-namePrinter('John', 'Doe');
+// namePrinter('Bob', 'Baxter');
+// namePrinter('John', 'Doe');
 
 
 const nuggetizer = (animal) => {
     return `processed ${animal}`;
 }
 
-console.log(nuggetizer('pig'));
+// console.log(nuggetizer('pig'));
 
 const dogBreed = (breed) => {
     return `my favorite dog breed is ${breed}`;
-}
+};
 
-console.log(dogBreed('border collie'));
+// console.log(dogBreed('border collie'));
+
+// const dogBreedDiv = document.getElementById('dog-breeds');
+// dogBreedDiv.innerHTML = dogBreed('bichon');
+// console.log('dogBreedDiv', dogBreedDiv);
+
+const nuggetizerDiv = document.getElementById('nuggets');
+nuggetizerDiv.innerHTML = nuggetizer('bear');
+nuggetizerDiv.innerHTML += nuggetizer('kitten');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+printToDom('dog-breeds', dogBreed('lab'));
+printToDom('nuggets', 'YUMMY');
