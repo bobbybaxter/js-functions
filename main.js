4d1604b99cc301a9d4355ca3cc8e50c348f06c74
Bob <bob.e.baxter@gmal.com>
2019-03-02 14:07:31 -0600

    created band list

--- main.js
+++ main.js
@@ -27,14 +27,28 @@ const dogBreed = (breed) => {
 // dogBreedDiv.innerHTML = dogBreed('bichon');
 // console.log('dogBreedDiv', dogBreedDiv);
 
-const nuggetizerDiv = document.getElementById('nuggets');
-nuggetizerDiv.innerHTML = nuggetizer('bear');
-nuggetizerDiv.innerHTML += nuggetizer('kitten');
+// const nuggetizerDiv = document.getElementById('nuggets');
+// nuggetizerDiv.innerHTML = nuggetizer('bear');
+// nuggetizerDiv.innerHTML += nuggetizer('kitten');
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += textToPrint;
 };
 
-printToDom('dog-breeds', dogBreed('lab'));
-printToDom('nuggets', 'YUMMY');
+// printToDom('dog-breeds', dogBreed('lab'));
+// printToDom('nuggets', 'YUMMY');
+
+
+
+let bandNumber = 1;
+const addBand = (bandName) => {
+    // do stuff
+    const listItem = `${bandNumber}. ${bandName}<br>`;
+    bandNumber++;
+    printToDom('band-list', listItem);
+}
+
+addBand('Metallica'); // 1. Metallica
+addBand('Slayer'); // 2. Slayer
+addBand('King Diamond');
