1c123ac615b7a9f64df0d158ecc03ff0451b846b
Bob <bob.e.baxter@gmal.com>
2019-03-02 11:07:21 -0600

    namePrinter functions works

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const firstName = 'Bob';
+const lastName = 'Baxter';
+// console.log(firstName + ' ' + lastName);
+// console.log(`${firstName} ${lastName}`);
+
+const namePrinter = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`)
+}
+
+namePrinter('Bob', 'Baxter');
+namePrinter('John', 'Doe');
